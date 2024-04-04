781498ec38c531c0fae1901457ed91c5d63780ff
Wijdan <wijdanbita@gmail.com>
2024-04-04 12:46:13 +0700

    init pertama

---
+++ src/projekpbo/ProjekPBO.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projekpbo;
+
+/**
+ *
+ * @author user
+ */
+public class ProjekPBO extends Kotak {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Testing\n");
+        // Init Class Parentnya
+        Kotak kt = new Kotak();
+        System.out.println("Dari Clas Parent = " + kt.nama);
+    }
+    
+}
